a3f2f56636bf157636cce8fc28fdca203d674cd2
Varun Kumar P <194123820+varunkumarp17@users.noreply.github.com>
2026-07-27 20:02:46 +0530

    Add practice23

---
+++ practice23/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number\n");
+scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("Number is even\n");
+    }
+    else
+    {
+        printf("Number is odd\n");
+    }
+
+    return 0;
+}
